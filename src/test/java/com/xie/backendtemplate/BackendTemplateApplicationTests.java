eb4a56f6987c8a8292ad89e5ca5efc2523014278
xiemingquan <1932576789@qq.com>
2023-06-29 20:44:41 +0800

    后端模板

---
+++ src/test/java/com/xie/backendtemplate/BackendTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xie.backendtemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
